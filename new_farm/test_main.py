764e9d23a18262c2de3d249917d74d3ad948febd
MariekeVermeij <cj.liefhebber@royalihc.com>
2022-07-30 12:55:33 +0000

    flask toegevoegd aan testfile

--- new_farm/test_main.py
+++ new_farm/test_main.py
@@ -1,12 +1,20 @@
 import main
+from flask import Flask
+import datetime
+
+
 
 def test_index():
     assert main.index(2) == "Good Night"
     assert main.index(5) == "Good Morning"
     assert main.index(18) == "Good Evening"
     assert main.index(17) == "Good Afternoon"
+
+
 def test_cow():
     assert main.cow() == 'Help a Cow to find a friend'
 
+
 def test_flying_cow():
     assert main.fly() == 'Whooooo ooooo ... HOW?! - a flying Cow!!'
+
