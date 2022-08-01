93d3599759587132c3a1486abecf1b1114ec92ce
MariekeVermeij <cj.liefhebber@royalihc.com>
2022-08-01 14:43:03 +0000

    final version app

--- new_farm/main.py
+++ new_farm/main.py
@@ -1,14 +1,24 @@
-# Import what we need from flask
-from flask import Flask
-
 # Create a Flask app inside `app`
 app = Flask(__name__)
-
+hour = datetime.datetime.now().hour
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
-def index():
-    return 'Hello, world!'
+def index(hour):
+    if hour >= 5 and hour < 12:
+        return "Good Morning"
+    elif hour >= 12 and hour < 18:
+        return "Good Afternoon"
+    elif (hour >= 18 and hour < 23):
+        return "Good Evening"
+    else:
+        return "Good Night"
+
 
 @app.route('/cow')
 def cow():
-    return 'MOoooOo!'
+    return 'Help a Cow to find a friend'
+
+@app.route('/fly')
+def fly():
+    return 'Whooooo ooooo ... HOW?! - a flying Cow!!'
+
--- new_farm/test_main.py
+++ new_farm/test_main.py
@@ -1,5 +1,12 @@
-import main
+import farm2
 
-
+def test_index():
+    assert farm2.index(2) == "Good Night"
+    assert farm2.index(5) == "Good Morning"
+    assert farm2.index(18) == "Good Evening"
+    assert farm2.index(17) == "Good Afternoon"
 def test_cow():
-    assert main.cow() == 'MOoooOo!'
+    assert farm2.cow() == 'Help a Cow to find a friend'
+
+def test_flying_cow():
+    assert farm2.fly() == 'Whooooo ooooo ... HOW?! - a flying Cow!!'
