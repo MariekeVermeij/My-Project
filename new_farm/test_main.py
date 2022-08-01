554facd1e9aa6e6e693e1d069a7f34c6d7307c7e
MariekeVermeij <cj.liefhebber@royalihc.com>
2022-08-01 15:29:46 +0000

    volgende keer eerst pauze nemen-nu zou hetgoed moeten zijn

--- new_farm/test_main.py
+++ new_farm/test_main.py
@@ -1,12 +1,12 @@
 import main
 
 def test_index():
-    assert farm2.index(2) == "Good Night"
-    assert farm2.index(5) == "Good Morning"
-    assert farm2.index(18) == "Good Evening"
-    assert farm2.index(17) == "Good Afternoon"
+    assert main.index(2) == "Good Night"
+    assert main.index(5) == "Good Morning"
+    assert main.index(18) == "Good Evening"
+    assert main.index(17) == "Good Afternoon"
 def test_cow():
-    assert farm2.cow() == 'Help a Cow to find a friend'
+    assert main.cow() == 'Help a Cow to find a friend'
 
 def test_flying_cow():
-    assert farm2.fly() == 'Whooooo ooooo ... HOW?! - a flying Cow!!'
+    assert main.fly() == 'Whooooo ooooo ... HOW?! - a flying Cow!!'
