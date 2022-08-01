bae54c5a1f39dc14e788b05cfe13a148cdc09ac0
MariekeVermeij <cj.liefhebber@royalihc.com>
2022-08-01 13:44:08 +0000

    cd voor mkdir weggehaald voor test

--- new_farm/script-farm.sh
+++ new_farm/script-farm.sh
@@ -1,7 +1,7 @@
 #if the test in actions work run the sh file this file copys new main file over current file
 
 cd $GITHUB_WORKSPACE
-cd mkdir vogel
+mkdir vogel
 cd ..
-cp ~/my-project/new_farm/main.py home/farm
+cp /my-project/new_farm/main.py home/farm
 systemctl restart farm
