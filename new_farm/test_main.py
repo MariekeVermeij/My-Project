0529d439b11f901c3944cb72585adddb4c91fcfb
MariekeVermeij <cj.liefhebber@royalihc.com>
2022-08-01 14:26:09 +0000

    test ook verkleind

--- new_farm/test_main.py
+++ new_farm/test_main.py
@@ -3,9 +3,6 @@ import main
 
 def test_index():
     assert main.index(2) == "Good Night"
-    assert main.index(5) == "Good Morning"
-    assert main.index(18) == "Good Evening"
-    assert main.index(17) == "Good Afternoon"
 
 
 def test_cow():
