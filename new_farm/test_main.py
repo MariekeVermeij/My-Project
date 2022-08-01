e2187315f9b640b2cb4127448ef9c4f2b8a7ebff
MariekeVermeij <cj.liefhebber@royalihc.com>
2022-08-01 14:34:23 +0000

    oude versie dus

--- new_farm/test_main.py
+++ new_farm/test_main.py
@@ -2,9 +2,4 @@ import main
 
 
 def test_cow():
-    assert main.cow() == 'Help a Cow to find a friend'
-
-
-def test_flying_cow():
-    assert main.fly() == 'Whooooo ooooo ... HOW?! - a flying Cow!!'
-
+    assert main.cow() == 'MOoooOo!'
